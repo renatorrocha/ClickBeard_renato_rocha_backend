f958788b469e21e205ada2a189b63f205bc0b301
renatorrocha <renatorrodrigues2002@gmail.com>
2025-05-07 15:35:48 -0300

    feat: update CORS configuration to use environment variable for frontend URL

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,12 @@ export const app = new Elysia()
   .use(swagger)
   .use(jwt)
   .use(logger())
-  .use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
-    credentials: true,
-  }))
+  .use(
+    cors({
+      origin: [Bun.env.FRONTEND_URL || 'http://localhost:5173'],
+      credentials: true,
+    })
+  )
   .use(routes)
 
   .listen(Bun.env.PORT || 3000);
